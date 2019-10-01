83983235907f820a3b04eb6e1d25a0ecd258dbb8
EsraaGK <esraagamalkhodeir@gmail.com>
2019-10-01 18:21:28 +0200

    first commit

--- UIExercise/ViewController.swift
+++ UIExercise/ViewController.swift
@@ -8,12 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,  UICollectionViewDelegate, UICollectionViewDataSource {
+    @IBOutlet weak var collectionview: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionview.dataSource = self
+        collectionview.delegate = self
+        
+        ( collectionview.collectionViewLayout as? UICollectionViewFlowLayout)?.scrollDirection = .horizontal
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+      return  10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+      let cell = collectionview.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath)
+        let img = cell.viewWithTag(2) as! UIImageView
+          img.image  = UIImage(named: "icons8-ninja-head-100")
+          (cell.viewWithTag(1) as! UILabel).text = "manshet"
+        return cell
+    }
+    
+
+   
 
 
 }
